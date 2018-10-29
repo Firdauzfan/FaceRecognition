e0d23d5bcfefdf479abb77e3ad2562d031a19878
gspeff <gspeff@gspeff-X442UQR>
2018-10-29 16:47:27 +0700

    add email notif

--- database.py
+++ database.py
@@ -10,8 +10,10 @@ import datetime
 import main_facerec
 import os
 from bot_notif import *
+from email_notif import *
 from notify_run import Notify
 import cv2
+import smtplib
 
 # Connect to the database
 def data(insertdata,kamera):
@@ -91,9 +93,7 @@ def datang(insertdata,kamera,status,frame):
                     text_terlambat = 'Kepada Human Resource Development, kami memberitahukan bahwa karyawan dengan nama %s , Hari ini datang %s pada tanggal dan pukul %s' %(insertdata,status,timestamp)
                     id_tele_terlambat='205017793'
                     send_message(text_terlambat, id_tele_terlambat, poto)
-
-
-
+                    main_email()
 
         # connection is not autocommit by default. So you must commit to save
         # your changes.
---
+++ email_notif.py
@@ -0,0 +1,68 @@
+import smtplib
+
+from string import Template
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+MY_ADDRESS = 'gspeintercon@gmail.com'
+PASSWORD = 'gspegspe'
+
+def get_contacts(filename):
+    """
+    Return two lists names, emails containing names and email addresses
+    read from a file specified by filename.
+    """
+
+    names = []
+    emails = []
+    with open(filename, mode='r', encoding='utf-8') as contacts_file:
+        for a_contact in contacts_file:
+            names.append(a_contact.split()[0])
+            emails.append(a_contact.split()[1])
+    return names, emails
+
+def read_template(filename):
+    """
+    Returns a Template object comprising the contents of the
+    file specified by filename.
+    """
+
+    with open(filename, 'r', encoding='utf-8') as template_file:
+        template_file_content = template_file.read()
+    return Template(template_file_content)
+
+def main_email():
+    names, emails = get_contacts('mycontacts.txt') # read contacts
+    message_template = read_template('message.txt')
+
+    # set up the SMTP server
+    s = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    s.ehlo()
+    s.login(MY_ADDRESS, PASSWORD)
+
+    # For each contact, send the email:
+    for name, email in zip(names, emails):
+        msg = MIMEMultipart()       # create a message
+
+        # add in the actual person name to the message template
+        message = message_template.substitute(PERSON_NAME=name.title())
+
+        # Prints out the message body for our sake
+        print(message)
+
+        # setup the parameters of the message
+        msg['From']=MY_ADDRESS
+        msg['To']=email
+        msg['Subject']="This is TEST"
+
+        # add in the message body
+        msg.attach(MIMEText(message, 'plain'))
+
+        message = 'Subject: {}\n\n{}'.format(msg['Subject'], message)
+        # send the message via the server set up earlier.
+        s.sendmail(msg['From'],msg['To'],message)
+        del msg
+
+    # Terminate the SMTP session and close the connection
+    s.close()
